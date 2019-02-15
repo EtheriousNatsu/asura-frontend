9f7168241104e5ef8de0cc07355ecd6345da99cf
EtheriousNatsu <zhouqiang847@gmail.com>
2019-02-15 17:05:53 +0800

    update footer

--- src/components/dashboard/DashboardFoot.vue
+++ src/components/dashboard/DashboardFoot.vue
@@ -9,7 +9,7 @@
             <b-col md="5">
               <b-nav>
                 <b-nav-text>
-                  2018 - 2019
+                  © 2018 - 2019 华海乐盈
                 </b-nav-text>
               </b-nav>
             </b-col>
@@ -24,10 +24,14 @@
                 md="5" 
                 class="">
               <b-nav class="justify-content-end" >
-                <b-nav-item to="/login">Login</b-nav-item>
+                <b-nav-text class="text-right">
+                  developer: 周强
+                </b-nav-text>
+                <!-- <b-nav-item to="/login">Contact</b-nav-item>
                 <b-nav-item 
-                    to="/signup" 
-                    style="width: 65px">Signup</b-nav-item>
+                  class="float-right"
+                  to="/signup" 
+                  style="width: 65px">Doc</b-nav-item> -->
               </b-nav>
             </b-col>
           </b-row>
